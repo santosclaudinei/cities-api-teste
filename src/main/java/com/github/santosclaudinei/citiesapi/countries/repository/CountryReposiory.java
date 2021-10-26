101d9512432205e3a8eeda619077c1c356aeb23f
Claudinei Santos <claudineibitta@gmail.com>
2021-10-26 11:04:16 -0300

    Primeiro commit

---
+++ src/main/java/com/github/santosclaudinei/citiesapi/countries/repository/CountryReposiory.java
@@ -0,0 +1,10 @@
+package com.github.santosclaudinei.citiesapi.countries.repository;
+
+import com.github.santosclaudinei.citiesapi.countries.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CountryReposiory extends JpaRepository <Country, Long> {
+
+
+
+}
---
+++ src/main/java/com/github/santosclaudinei/citiesapi/states/State.java
@@ -0,0 +1,70 @@
+package com.github.santosclaudinei.citiesapi.states;
+
+import com.github.santosclaudinei.citiesapi.countries.Country;
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity(name = "State")
+@Table(name = "estado")
+    @TypeDefs({
+            @TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
+    })
+
+public class State {
+
+    @Id
+    private Long id;
+
+    @Column(name = "nome")
+    private String name;
+
+    private String uf;
+
+    private Integer ibge;
+
+//    @Column(name = "pais")
+//    private Integer CountryId;
+
+    // 2nd - @ManyToOne
+    @ManyToOne
+    @JoinColumn(name = "pais", referencedColumnName = "id")
+    private Country country;
+
+    @Type(type = "jsonb")
+    @Basic(fetch = FetchType.LAZY)
+    @Column(name = "ddd", columnDefinition = "jsonb")
+    private List<Integer> ddd;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public Integer getIbge() {
+        return ibge;
+    }
+
+    public List<Integer> getDdd() {
+        return ddd;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+//    public Integer getCountryId() {
+//        return CountryId;
+//    }
+}
